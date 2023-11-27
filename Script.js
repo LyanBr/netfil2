624f4a73192d36242796954748cc223f9fcd3ff4
Netossim <120585455+Netossim@users.noreply.github.com>
2023-11-27 14:08:21 -0300

    Update Script.js
    adicionando comentarios

--- Script.js
+++ Script.js
@@ -1,62 +1,89 @@
+// Seleciona todos os itens de lista (li) dentro da lista não ordenada (ul) dentro da classe 'div_header'.
 const menuItems = document.querySelectorAll('.div_header ul li');
-        
-                menuItems.forEach(item => {
-                    item.addEventListener('mouseenter', () => {
-                        item.style.transform = 'translateY(-5px)';
-                    });
-        
-                    item.addEventListener('mouseleave', () => {
-                        item.style.transform = 'translateY(0)';
-                    });
-                });
-                const searchInput = document.getElementById('search-input');
-                
-                searchInput.addEventListener('click', () => {
-                    searchInput.style.background = '#fff';
-                    searchInput.style.color = '#000';
-                });
-        
-                searchInput.addEventListener('blur', () => {
-                    searchInput.style.background = '#333';
-                    searchInput.style.color = '#fff';
-                });
-                const searchButton = document.getElementById('search-button');
-            searchButton.addEventListener('click', () => {
-                    searchButton.classList.add('search-animation'); /* Adicionamos a classe da animação */
-                    setTimeout(() => {
-                        searchButton.classList.remove('search-animation'); /* Removemos a classe após a animação */
-                    }, 200);
-                });
 
-// JavaScript no arquivo "index.html"
+// Para cada item de menu, adiciona dois ouvintes de eventos (mouseenter e mouseleave).
+menuItems.forEach(item => {
+    // Quando o mouse entra no item, move o item para cima (-5px) para criar um efeito visual.
+    item.addEventListener('mouseenter', () => {
+        item.style.transform = 'translateY(-5px)';
+    });
+
+    // Quando o mouse sai do item, move o item de volta para a posição original (0px).
+    item.addEventListener('mouseleave', () => {
+        item.style.transform = 'translateY(0)';
+    });
+});
+
+// Seleciona o elemento de input de pesquisa com o ID 'search-input'.
+const searchInput = document.getElementById('search-input');
+
+// Adiciona dois ouvintes de eventos para o input de pesquisa (clique e blur).
+searchInput.addEventListener('click', () => {
+    // Quando o input de pesquisa é clicado, muda o estilo para fundo branco e texto preto.
+    searchInput.style.background = '#fff';
+    searchInput.style.color = '#000';
+});
+
+searchInput.addEventListener('blur', () => {
+    // Quando o input de pesquisa perde o foco, muda o estilo de volta para fundo escuro e texto claro.
+    searchInput.style.background = '#333';
+    searchInput.style.color = '#fff';
+});
+
+// Seleciona o botão de pesquisa com o ID 'search-button'.
+const searchButton = document.getElementById('search-button');
+
+// Adiciona um ouvinte de evento para o clique no botão de pesquisa.
+searchButton.addEventListener('click', () => {
+    // Adiciona a classe 'search-animation' ao botão para iniciar uma animação.
+    searchButton.classList.add('search-animation');
+    
+    // Define um temporizador para remover a classe 'search-animation' após 200 milissegundos (0,2 segundos).
+    setTimeout(() => {
+        searchButton.classList.remove('search-animation');
+    }, 200);
+});
+// Aguarda o evento DOMContentLoaded para garantir que o HTML da página foi totalmente carregado.
 document.addEventListener("DOMContentLoaded", function () {
+    // Seleciona todos os elementos com a classe CSS "movie" e os armazena em uma NodeList.
     const movieContainers = document.querySelectorAll(".movie");
 
+    // Para cada elemento na NodeList "movieContainers".
     movieContainers.forEach((container) => {
+        // Adiciona um ouvinte de evento para o evento de clique em cada elemento "movie".
         container.addEventListener("click", () => {
+            // Obtém o valor do atributo 'data-filme' do elemento clicado.
             const filmeSelecionado = container.dataset.filme;
 
-            // Redirecionar para a página de templatefilme.html com o filme selecionado como parâmetro
+            // Redireciona para a página de templatefilme.html com o filme selecionado como parâmetro na URL.
             window.location.href = `templatefilme.html?filme=${filmeSelecionado}`;
         });
     });
 });
-
+// Aguarda o evento DOMContentLoaded para garantir que o HTML da página foi totalmente carregado.
 document.addEventListener("DOMContentLoaded", function () {
+    // Seleciona todos os elementos com a classe CSS "movie" e armazena-os em uma NodeList.
     const movieContainers = document.querySelectorAll(".movie");
+
+    // Obtém o elemento com o ID "destaque-title" e armazena-o em destaqueTitle.
     const destaqueTitle = document.getElementById("destaque-title");
 
+    // Função para filtrar e exibir filmes com base na categoria fornecida.
     function filterMovies(category) {
+        // Itera sobre cada elemento "movie" na NodeList.
         movieContainers.forEach(container => {
+            // Obtém o valor do atributo 'data-tag' de cada container.
             const tag = container.getAttribute("data-tag");
-    
+
+            // Verifica se a categoria é "inicio" ou corresponde à tag do container.
             if (category === "inicio" || category === tag) {
-                container.style.display = "inline-block"; // Altera para "inline-block"
+                container.style.display = "inline-block"; // Exibe o container.
             } else {
-                container.style.display = "none";
+                container.style.display = "none"; // Oculta o container.
             }
         });
-    
+
+        // Atualiza o texto do elemento com o ID "destaque-title" com base na categoria selecionada.
         if (category === "serie") {
             destaqueTitle.textContent = "Séries em Destaque";
         } else if (category === "filme") {
@@ -65,8 +92,8 @@ document.addEventListener("DOMContentLoaded", function () {
             destaqueTitle.textContent = "Filmes/Séries em Destaque";
         }
     }
-    
 
+    // Adiciona ouvintes de evento para os botões de filtro e chama a função filterMovies com a categoria correspondente.
     document.getElementById("filtro-inicio").addEventListener("click", () => {
         filterMovies("inicio");
     });
@@ -79,45 +106,54 @@ document.addEventListener("DOMContentLoaded", function () {
         filterMovies("filme");
     });
 });
+// Aguarda o evento DOMContentLoaded para garantir que o HTML da página foi totalmente carregado.
 document.addEventListener("DOMContentLoaded", function() {
+    // Obtém referências aos elementos HTML com os IDs "filme1" e "destino".
     var divParaMover = document.getElementById("filme1");
     var destino = document.getElementById("destino");
 
+    // Verifica se ambos os elementos existem.
     if (divParaMover && destino) {
-        // Mover a div de uma página para outra
+        // Move a div identificada por "filme1" para o elemento com ID "destino".
         destino.appendChild(divParaMover);
     }
 });
-
-
-
-
-
+// Obtém referências aos elementos HTML com os IDs "search-button", "search-input" e "resultados-container".
 const searchButto = document.getElementById('search-button');
 const searchInpu = document.getElementById('search-input');
 const resultadosContainer = document.getElementById('resultados-container');
 
+// Adiciona um ouvinte de evento para o clique no botão de pesquisa.
 searchButto.addEventListener('click', () => {
+    // Obtém o valor do campo de entrada de pesquisa.
     const searchTerm = searchInpu.value;
 
+    // Realiza uma requisição fetch para buscar filmes com base no termo de pesquisa.
     fetch(`/buscar-filmes?term=${searchTerm}`)
         .then((response) => response.json())
         .then((resultados) => {
+            // Exibe os resultados chamando a função exibirResultados.
             exibirResultados(resultados);
         })
         .catch((error) => {
+            // Em caso de erro, exibe uma mensagem de erro no console.
             console.error('Erro:', error);
         });
 });
 
+// Função que exibe os resultados na página.
 function exibirResultados(resultados) {
-    resultadosContainer.innerHTML = ''; // Limpar resultados anteriores
+    // Limpa quaisquer resultados anteriores no contêiner.
+    resultadosContainer.innerHTML = '';
 
+    // Verifica se há resultados.
     if (resultados.length === 0) {
+        // Se não houver resultados, exibe uma mensagem indicando isso.
         resultadosContainer.innerHTML = '<p>Nenhum filme encontrado.</p>';
         return;
     }
 
+    // Itera sobre os resultados e cria elementos HTML para cada filme, adicionando-os ao contêiner.
     resultados.forEach((filme) => {
         const filmeDiv = document.createElement('div');
         filmeDiv.className = 'movie';
@@ -127,6 +163,7 @@ function exibirResultados(resultados) {
         resultadosContainer.appendChild(filmeDiv);
     });
 }
+// Define um array representando o código Konami.
 const konamiCode = [
     'ArrowUp',
     'ArrowUp',
@@ -141,44 +178,55 @@ const konamiCode = [
 ];
 let konamiCodePosition = 0;
 
+// Função para verificar se a sequência do código Konami foi inserida.
 function checkKonamiCode(event) {
     const key = event.key;
 
+    // Verifica se a tecla pressionada corresponde à próxima posição no código Konami.
     if (key === konamiCode[konamiCodePosition]) {
         konamiCodePosition++;
 
+        // Se a sequência completa foi inserida, chama a função openCalculator.
         if (konamiCodePosition === konamiCode.length) {
             openCalculator();
             konamiCodePosition = 0;
         }
     } else {
+        // Reinicia a verificação se a tecla pressionada não corresponde à posição esperada no código Konami.
         konamiCodePosition = 0;
     }
 }
 
+// Função que é chamada ao inserir corretamente o código Konami, redirecionando para 'calculadora.html'.
 function openCalculator() {
-    window.location.href = 'calculadora.html'
+    window.location.href = 'calculadora.html';
     // Neste exemplo, exibimos um alerta como mensagem.
     alert("Código secreto detectado. Abrindo a calculadora.");
 }
 
+// Adiciona um ouvinte de evento para o evento de tecla pressionada.
 document.addEventListener('keydown', checkKonamiCode);
+// Funções para compartilhar o site nas redes sociais.
+
+// Compartilha no Facebook.
 function shareOnFacebook() {
-    // Substitua a URL pelo link que deseja compartilhar no Facebook
+    // Substitua a URL pelo link que deseja compartilhar no Facebook.
     var url = "https://lyanbr.github.io/netfil2/index.html";
     window.open("https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url), "_blank");
 }
 
+// Compartilha no Twitter.
 function shareOnTwitter() {
-    // Substitua a URL, texto e hashtags conforme necessário
+    // Substitua a URL, texto e hashtags conforme necessário.
     var url = "https://lyanbr.github.io/netfil2/index.html";
     var text = "Confira este incrível site! Feito Pelos melhores Programers do Brasil, @luanbraune @netossim";
     var hashtags = "webdev,SPIIFTO,teste";
     window.open("https://twitter.com/intent/tweet?url=" + encodeURIComponent(url) + "&text=" + encodeURIComponent(text) + "&hashtags=" + encodeURIComponent(hashtags), "_blank");
 }
+
+// Compartilha no WhatsApp.
 function shareOnWhatsApp() {
-    // Substitua o número e a mensagem conforme necessário
-    
+    // Substitua o número e a mensagem conforme necessário.
     var text = "Confira este incrível site: https://lyanbr.github.io/netfil2/index.html";
     window.open("https://wa.me/" + "?text=" + encodeURIComponent(text), "_blank");
 }
