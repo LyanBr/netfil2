0b5cf9f089de5c06ac6df2855ae3d13230c61415
Netossim <120585455+Netossim@users.noreply.github.com>
2023-11-28 19:50:50 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,34 +1,46 @@
+// Efeito de hover nos itens do menu
 const menuItems = document.querySelectorAll('.div_header ul li');
-        
-                menuItems.forEach(item => {
-                    item.addEventListener('mouseenter', () => {
-                        item.style.transform = 'translateY(-5px)';
-                    });
-        
-                    item.addEventListener('mouseleave', () => {
-                        item.style.transform = 'translateY(0)';
-                    });
-                });
-                const searchInput = document.getElementById('search-input');
-                
-                searchInput.addEventListener('click', () => {
-                    searchInput.style.background = '#fff';
-                    searchInput.style.color = '#000';
-                });
-        
-                searchInput.addEventListener('blur', () => {
-                    searchInput.style.background = '#333';
-                    searchInput.style.color = '#fff';
-                });
-                const searchButton = document.getElementById('search-button');
-            searchButton.addEventListener('click', () => {
-                    searchButton.classList.add('search-animation'); /* Adicionamos a classe da animação */
-                    setTimeout(() => {
-                        searchButton.classList.remove('search-animation'); /* Removemos a classe após a animação */
-                    }, 200);
-                });
 
-// JavaScript no arquivo "index.html"
+menuItems.forEach(item => {
+    // Ao passar o mouse sobre o item, desloca para cima
+    item.addEventListener('mouseenter', () => {
+        item.style.transform = 'translateY(-5px)';
+    });
+
+    // Ao retirar o mouse, volta à posição original
+    item.addEventListener('mouseleave', () => {
+        item.style.transform = 'translateY(0)';
+    });
+});
+
+// Mudança de estilo ao clicar no campo de pesquisa
+const searchInput = document.getElementById('search-input');
+
+searchInput.addEventListener('click', () => {
+    searchInput.style.background = '#fff';
+    searchInput.style.color = '#000';
+});
+
+// Mudança de estilo ao sair do campo de pesquisa
+searchInput.addEventListener('blur', () => {
+    searchInput.style.background = '#333';
+    searchInput.style.color = '#fff';
+});
+
+// Animação do botão de pesquisa
+const searchButton = document.getElementById('search-button');
+
+searchButton.addEventListener('click', () => {
+    // Adiciona classe de animação
+    searchButton.classList.add('search-animation');
+    
+    // Remove a classe após 200 milissegundos (final da animação)
+    setTimeout(() => {
+        searchButton.classList.remove('search-animation');
+    }, 200);
+});
+
+// Redirecionamento ao clicar em um contêiner de filme
 document.addEventListener("DOMContentLoaded", function () {
     const movieContainers = document.querySelectorAll(".movie");
 
@@ -36,12 +48,13 @@ document.addEventListener("DOMContentLoaded", function () {
         container.addEventListener("click", () => {
             const filmeSelecionado = container.dataset.filme;
 
-            // Redirecionar para a página de templatefilme.html com o filme selecionado como parâmetro
+            // Redireciona para a página de templatefilme.html com o filme selecionado como parâmetro
             window.location.href = `templatefilme.html?filme=${filmeSelecionado}`;
         });
     });
 });
 
+// Filtragem de filmes por categoria
 document.addEventListener("DOMContentLoaded", function () {
     const movieContainers = document.querySelectorAll(".movie");
     const destaqueTitle = document.getElementById("destaque-title");
@@ -49,14 +62,16 @@ document.addEventListener("DOMContentLoaded", function () {
     function filterMovies(category) {
         movieContainers.forEach(container => {
             const tag = container.getAttribute("data-tag");
-    
+
+            // Exibe ou oculta conforme a categoria
             if (category === "inicio" || category === tag) {
-                container.style.display = "inline-block"; // Altera para "inline-block"
+                container.style.display = "inline-block";
             } else {
                 container.style.display = "none";
             }
         });
-    
+
+        // Altera o título conforme a categoria selecionada
         if (category === "serie") {
             destaqueTitle.textContent = "Séries em Destaque";
         } else if (category === "filme") {
@@ -65,8 +80,8 @@ document.addEventListener("DOMContentLoaded", function () {
             destaqueTitle.textContent = "Filmes/Séries em Destaque";
         }
     }
-    
 
+    // Adiciona ouvintes de evento para os botões de filtro
     document.getElementById("filtro-inicio").addEventListener("click", () => {
         filterMovies("inicio");
     });
@@ -79,6 +94,8 @@ document.addEventListener("DOMContentLoaded", function () {
         filterMovies("filme");
     });
 });
+
+// Mover a div "filme1" para outra página
 document.addEventListener("DOMContentLoaded", function() {
     var divParaMover = document.getElementById("filme1");
     var destino = document.getElementById("destino");
@@ -89,10 +106,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-
-
-
-
+// Busca de filmes
 const searchButto = document.getElementById('search-button');
 const searchInpu = document.getElementById('search-input');
 const resultadosContainer = document.getElementById('resultados-container');
@@ -110,6 +124,7 @@ searchButto.addEventListener('click', () => {
         });
 });
 
+// Função para exibir resultados na página
 function exibirResultados(resultados) {
     resultadosContainer.innerHTML = ''; // Limpar resultados anteriores
 
@@ -127,6 +142,8 @@ function exibirResultados(resultados) {
         resultadosContainer.appendChild(filmeDiv);
     });
 }
+
+// Código Konami para abrir a calculadora
 const konamiCode = [
     'ArrowUp',
     'ArrowUp',
@@ -156,13 +173,17 @@ function checkKonamiCode(event) {
     }
 }
 
+// Função para abrir a calculadora
 function openCalculator() {
     window.location.href = 'calculadora.html'
     // Neste exemplo, exibimos um alerta como mensagem.
     alert("Código secreto detectado. Abrindo a calculadora.");
 }
 
+// Adiciona um ouvinte de evento para a tecla pressionada
 document.addEventListener('keydown', checkKonamiCode);
+
+// Funções para compartilhar nas redes sociais
 function shareOnFacebook() {
     // Substitua a URL pelo link que deseja compartilhar no Facebook
     var url = "https://lyanbr.github.io/netfil2/index.html";
@@ -176,9 +197,9 @@ function shareOnTwitter() {
     var hashtags = "webdev,SPIIFTO,teste";
     window.open("https://twitter.com/intent/tweet?url=" + encodeURIComponent(url) + "&text=" + encodeURIComponent(text) + "&hashtags=" + encodeURIComponent(hashtags), "_blank");
 }
+
 function shareOnWhatsApp() {
     // Substitua o número e a mensagem conforme necessário
-    
     var text = "Confira este incrível site: https://lyanbr.github.io/netfil2/index.html";
     window.open("https://wa.me/" + "?text=" + encodeURIComponent(text), "_blank");
 }
